e9f27f0191ddfd7d75e3622491e967f77f3b0e28
ABSphreak <absphreak@outlook.com>
2022-01-18 01:47:28 +0530

    ➕ Add mock data

---
+++ data/mock.js
@@ -0,0 +1,21 @@
+export const mockTasks = [
+	{
+		id: 1,
+		text: 'Task 1',
+		belongsTo: 'Backlog',
+	},
+];
+
+export const mockGroups = [
+	{
+		id: 1,
+		title: 'Backlog',
+		tasks: [
+			{
+				id: 1,
+				text: 'Task 1',
+				belongsTo: 'Backlog',
+			},
+		],
+	},
+];

879a667d9b94aee9730809593c21ee2a4b2a19bf
ABSphreak <absphreak@outlook.com>
2022-02-04 03:06:33 +0530

    ➕ Added drag logic

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,8 +1,31 @@
-const TaskCard = ({ text }: { text: string }) => {
+import { useState } from 'react';
+import TaskEditModal from './TaskEditModal';
+import { DotsHorizontalIcon } from '@heroicons/react/solid';
+import { Task } from '../data/types';
+
+const TaskCard = ({ task }: { task: Task }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+		e.dataTransfer.setData('task', JSON.stringify(task));
+		e.currentTarget.classList.add('opacity-50');
+	};
+
 	return (
-		<div className="px-3 py-3 my-2 text-left transition-shadow duration-100 border border-gray-700 rounded-sm shadow-sm select-none hover:cursor-grab active:cursor-grabbing w-60 shadow-gray-600 active:shadow-gray-200">
-			{text}
-		</div>
+		<>
+			<div
+				draggable
+				onDragStart={onDragStart}
+				className="relative px-3 py-3 my-2 text-left transition-shadow duration-100 border border-gray-700 rounded-sm shadow-sm select-none hover:cursor-grab active:cursor-grabbing w-60 shadow-gray-600 active:shadow-gray-200">
+				<div>{task.text}</div>
+				<div className="absolute top-0 right-[3px]">
+					<button className="px-2 py-1 text-gray-500 rounded-sm shadow-sm" onClick={() => setIsOpen(true)}>
+						<DotsHorizontalIcon className="w-5 h-5" />
+					</button>
+				</div>
+			</div>
+			<TaskEditModal isOpen={isOpen} setIsOpen={setIsOpen} task={task} />
+		</>
 	);
 };
 
--- components/TasksGroup.tsx
+++ components/TasksGroup.tsx
@@ -1,34 +1,46 @@
+import { useContext, useState } from 'react';
 import TaskCard from './TaskCard';
+import TaskAddModal from './TaskAddModal';
 import { PlusIcon } from '@heroicons/react/solid';
 import { maxContent } from '../utils';
+import { Task, TaskGroup } from '../data/types';
+import { TaskGroupContext } from '../pages';
 
-type TaskGroupType = {
-	id: number;
-	title: string;
-	tasks: {
-		id: number;
-		text: string;
-		belongsTo: string;
-	}[];
-};
+const TasksGroup = ({ taskGroup }: { taskGroup: TaskGroup }) => {
+	const [taskAddOpen, setTaskAddOpen] = useState(false);
+	const { moveTask } = useContext(TaskGroupContext);
 
-const TasksGroup = ({ taskGroup }: { taskGroup: TaskGroupType }) => {
+	const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+		e.preventDefault();
+	};
+
+	const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+		let task = JSON.parse(e.dataTransfer.getData('task'));
+		moveTask?.(task.belongsTo, taskGroup.id, task);
+	};
+
 	return (
-		<div className="">
-			<div className="mb-5">
-				<div className="text-xl font-bold text-left w-60">{maxContent(`${taskGroup.title}`, 20)}</div>
-			</div>
-			<div>
-				{taskGroup.tasks.map(task => (
-					<TaskCard key={task.id} text={task.text} />
-				))}
-				<div className="my-2 text-center transition-shadow duration-100 border border-gray-700 border-dashed rounded-sm shadow-sm select-none w-60">
-					<button className="flex items-center justify-center w-full h-full gap-0.5 px-3 py-2 text-gray-500">
-						Add Task <PlusIcon className="w-4 h-4" />
-					</button>
+		<>
+			<div className="" onDragOver={onDragOver} onDrop={onDrop}>
+				<div className="mb-5">
+					<div className="text-xl font-bold text-left w-60">{maxContent(`${taskGroup.title}`, 20)}</div>
+				</div>
+				<div>
+					{taskGroup.tasks.map((task: Task) => (
+						<TaskCard key={task.id} task={task} />
+					))}
+					<div className="my-2 text-center transition-shadow duration-100 border border-gray-700 border-dashed rounded-sm shadow-sm select-none w-60">
+						<button
+							type="button"
+							onClick={() => setTaskAddOpen(true)}
+							className="flex items-center justify-center w-full h-full gap-0.5 px-3 py-2 text-gray-500">
+							Add Task <PlusIcon className="w-4 h-4" />
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
+			<TaskAddModal setIsOpen={setTaskAddOpen} isOpen={taskAddOpen} groupId={taskGroup.id} />
+		</>
 	);
 };
 

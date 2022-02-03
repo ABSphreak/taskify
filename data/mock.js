e3c0b421e4c707099f2052f6f5efd110bcb581d9
ABSphreak <absphreak@outlook.com>
2022-02-04 03:07:44 +0530

    ➕ Add initial data

--- data/mock.js
+++ data/mock.js
@@ -1,20 +1,35 @@
-export const mockTasks = [
-	{
-		id: 1,
-		text: 'Task 1',
-		belongsTo: 'Backlog',
-	},
-];
+import { v4 as uuidv4 } from 'uuid';
 
 export const mockGroups = [
 	{
-		id: 1,
+		id: uuidv4(),
 		title: 'Backlog',
 		tasks: [
 			{
-				id: 1,
+				id: uuidv4(),
 				text: 'Task 1',
 				belongsTo: 'Backlog',
+			},
+			{
+				id: uuidv4(),
+				text: 'Task 2',
+				belongsTo: 'Backlog',
+			},
+		],
+	},
+	{
+		id: uuidv4(),
+		title: 'In Progress',
+		tasks: [
+			{
+				id: uuidv4(),
+				text: 'Task 3',
+				belongsTo: 'In Progress',
+			},
+			{
+				id: uuidv4(),
+				text: 'Task 4',
+				belongsTo: 'In Progress',
 			},
 		],
 	},
